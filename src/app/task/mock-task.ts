55f1513a5a28a6bd8314bdc9b099deecc33e4ce9
Wilfred <wilfred5777@gmail.com>
2021-05-30 17:13:31 +0800

    tasks mocks and task components

---
+++ src/app/task/mock-task.ts
@@ -0,0 +1,34 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School Appointment',
+    day: 'June 5th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Food Shopping Appointment',
+    day: 'July 16th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Going to Tagum Appointment',
+    day: 'May 25th at 2:30pm',
+    reminder: false,
+  },
+];
