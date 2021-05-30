096d325493e6245bbf04c833c2a84d0bc1ae8f2d
Wilfred <wilfred5777@gmail.com>
2021-05-30 16:53:48 +0800

    button and header

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
